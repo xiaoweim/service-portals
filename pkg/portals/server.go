4ba39939e700fe05f3941ac1eb4381680331d480
codebot-robot <codebot-robot@google.com>
2026-04-18 14:03:44 +0000

    Refactor core proxy running logic into pkg/portals

---
+++ pkg/portals/server.go
@@ -0,0 +1,106 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package portals
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/gke-labs/service-portals/pkg/proxy"
+)
+
+type Config struct {
+	DefaultTargetURL  string
+	DefaultAuthHeader string
+	SetupProxy        func(*proxy.HTTPProxy)
+}
+
+func Run(ctx context.Context, config Config) error {
+	target := os.Getenv("TARGET_URL")
+	if target == "" {
+		target = config.DefaultTargetURL
+	}
+	if target == "" {
+		target = "https://generativelanguage.googleapis.com"
+	}
+
+	upstreamAuthToken := os.Getenv("UPSTREAM_AUTH_TOKEN")
+	if upstreamAuthToken == "" {
+		log.Println("Warning: UPSTREAM_AUTH_TOKEN is not set. No Authorization header will be injected.")
+	}
+
+	upstreamAuthHeader := os.Getenv("UPSTREAM_AUTH_HEADER")
+	if upstreamAuthHeader == "" {
+		if config.DefaultAuthHeader != "" {
+			upstreamAuthHeader = config.DefaultAuthHeader
+		} else {
+			upstreamAuthHeader = "Authorization"
+		}
+	}
+
+	targetURL, err := url.Parse(target)
+	if err != nil {
+		return fmt.Errorf("invalid TARGET_URL: %w", err)
+	}
+
+	caCertPath := os.Getenv("CA_CERT_PATH")
+	caKeyPath := os.Getenv("CA_KEY_PATH")
+
+	p, err := proxy.NewHTTPProxy(targetURL, upstreamAuthToken, upstreamAuthHeader, caCertPath, caKeyPath)
+	if err != nil {
+		return fmt.Errorf("failed to create proxy: %w", err)
+	}
+
+	if config.SetupProxy != nil {
+		config.SetupProxy(p)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: p,
+	}
+
+	errChan := make(chan error, 1)
+	go func() {
+		log.Printf("Starting proxy on :%s forwarding to %s", port, target)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			errChan <- fmt.Errorf("server failed: %w", err)
+		}
+	}()
+
+	select {
+	case err := <-errChan:
+		return err
+	case <-ctx.Done():
+		log.Println("Shutting down server...")
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			return fmt.Errorf("server shutdown failed: %w", err)
+		}
+	}
+
+	return nil
+}
